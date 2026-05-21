01851957961c980e5c8b8fdb50598055123339f3
liuweiliang98 <907622645@qq.com>
2026-05-21 23:04:28 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  siteName: '龚仔的摄影日记',
+  siteName: '伟良的摄影日记',
   siteTitle: '记录生活，定格美好',
   siteDescription: '用镜头捕捉每一个值得珍藏的瞬间',
   footer: {
-    html: '© 2026 龚仔的摄影日记 | <a href="https://v.douyin.com/Hc74k9FgjPY/" target="_blank">@龚仔仔仔仔</a>'
+    html: '© 2026 伟良的摄影日记 | <a href="https://www.douyin.com/user/MS4wLjABAAAAK5w6BUnXJrqE1KZs9_G080KQH9-0H0XpTVjueD_-zKw?from_tab_name=main" target="_blank">@伟良</a>'
   },
   contacts: {
     douyin: 'https://v.douyin.com/Hc74k9FgjPY/',
